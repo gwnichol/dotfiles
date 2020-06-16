399b05f01e43344ddc4134fb6adc1caaeef39bbe
Grant Nichol <gwnichol.m@gmail.com>
2020-06-16 00:54:00 -0500

    Simplify theme definition

--- .config/awesome/badwolf/theme.lua
+++ .config/awesome/badwolf/theme.lua
@@ -7,9 +7,10 @@ local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_configuration_dir() .. "badwolf/"
-local default_theme = "/usr/share/awesome/themes/default/"
+local global_themes = gfs.get_themes_dir()
 
-local theme = {}
+-- Derive theme from default
+local theme = dofile(global_themes .. "default/theme.lua")
 
 theme.font          = "sans 8"
 
@@ -67,7 +68,6 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = default_theme .. "submenu.png"
 theme.menu_height = dpi(10)
 theme.menu_width  = dpi(100)
 
@@ -75,52 +75,6 @@ theme.menu_width  = dpi(100)
 -- you wish and access them by using
 -- beautiful.variable in your rc.lua
 --theme.bg_widget = "#cc0000"
-
--- Define the image to load
-theme.titlebar_close_button_normal = default_theme.."titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = default_theme.."titlebar/close_focus.png"
-
-theme.titlebar_minimize_button_normal = default_theme.."titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = default_theme.."titlebar/minimize_focus.png"
-
-theme.titlebar_ontop_button_normal_inactive = default_theme.."titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = default_theme.."titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = default_theme.."titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = default_theme.."titlebar/ontop_focus_active.png"
-
-theme.titlebar_sticky_button_normal_inactive = default_theme.."titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = default_theme.."titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = default_theme.."titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = default_theme.."titlebar/sticky_focus_active.png"
-
-theme.titlebar_floating_button_normal_inactive = default_theme.."titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = default_theme.."titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = default_theme.."titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = default_theme.."titlebar/floating_focus_active.png"
-
-theme.titlebar_maximized_button_normal_inactive = default_theme.."titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = default_theme.."titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = default_theme.."titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = default_theme.."titlebar/maximized_focus_active.png"
-
-
--- You can use your own layout icons like this:
-theme.layout_fairh = default_theme.."layouts/fairhw.png"
-theme.layout_fairv = default_theme.."layouts/fairvw.png"
-theme.layout_floating  = default_theme.."layouts/floatingw.png"
-theme.layout_magnifier = default_theme.."layouts/magnifierw.png"
-theme.layout_max = default_theme.."layouts/maxw.png"
-theme.layout_fullscreen = default_theme.."layouts/fullscreenw.png"
-theme.layout_tilebottom = default_theme.."layouts/tilebottomw.png"
-theme.layout_tileleft   = default_theme.."layouts/tileleftw.png"
-theme.layout_tile = default_theme.."layouts/tilew.png"
-theme.layout_tiletop = default_theme.."layouts/tiletopw.png"
-theme.layout_spiral  = default_theme.."layouts/spiralw.png"
-theme.layout_dwindle = default_theme.."layouts/dwindlew.png"
-theme.layout_cornernw = default_theme.."layouts/cornernww.png"
-theme.layout_cornerne = default_theme.."layouts/cornernew.png"
-theme.layout_cornersw = default_theme.."layouts/cornersww.png"
-theme.layout_cornerse = default_theme.."layouts/cornersew.png"
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
